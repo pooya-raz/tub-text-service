fde3d11b6b8909f3d575acecdb2718e4f3e58dc6
Christopher Pooya Razavian <pooya72@gmail.com>
2022-12-19 18:23:18 +0000

    Match json keys

--- src/main/java/org/tub/tubtextservice/model/tubresponse/Author.java
+++ src/main/java/org/tub/tubtextservice/model/tubresponse/Author.java
@@ -1,8 +1,4 @@
 package org.tub.tubtextservice.model.tubresponse;
 
 public record Author(
-    String fullText,
-    String fullUrl,
-    Integer namespace,
-    String exists,
-    String displayTitle) {}
+    String fulltext, String fullurl, Integer namespace, String exists, String displaytitle) {}
