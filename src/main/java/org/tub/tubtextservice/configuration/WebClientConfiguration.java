7d3c3b6dc85f72feac3dacfc85f448c9f6163b28
Christopher Pooya Razavian <pooya72@gmail.com>
2023-01-07 14:05:39 +0000

    Increase WebClient memory and wait time

--- src/main/java/org/tub/tubtextservice/configuration/WebClientConfiguration.java
+++ src/main/java/org/tub/tubtextservice/configuration/WebClientConfiguration.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
@@ -11,7 +12,10 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import org.tub.tubtextservice.client.TubClient;
 import org.tub.tubtextservice.model.property.TubProperties;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfiguration {
@@ -25,18 +29,22 @@ public class WebClientConfiguration {
 
   @Bean
   TubClient tubClient(final WebClient.Builder builder) {
-    final WebClient webClient =
+    final int size = 20 * 1024 * 1024; // 12 MB
+    final var client = HttpClient.create().responseTimeout(Duration.ofMinutes(2));
+    final var webClient =
         builder
             .baseUrl(tubProperties.apiUrl())
-            .filter(logRequest("Fetching data from TUB: "))
-            .filter(retryFilter("Retrying to fetch data from TUB", tubProperties))
+            .filter(logRequest())
+            .filter(retryFilter(tubProperties))
+            .clientConnector(new ReactorClientHttpConnector(client))
+            .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
             .build();
-    final HttpServiceProxyFactory factory =
+    final var factory =
         HttpServiceProxyFactory.builder(WebClientAdapter.forClient(webClient)).build();
     return factory.createClient(TubClient.class);
   }
 
-  private ExchangeFilterFunction retryFilter(String text, TubProperties properties) {
+  private ExchangeFilterFunction retryFilter(TubProperties properties) {
     return (request, next) ->
         next.exchange(request)
             .flatMap(
@@ -48,12 +56,12 @@ public class WebClientConfiguration {
                         .thenReturn(clientResponse))
             .retryWhen(
                 Retry.backoff(properties.retryMaxAttempts(), properties.retryBackoffPeriod())
-                    .doBeforeRetry(retrySignal -> log.warn(text)));
+                    .doBeforeRetry(retrySignal -> log.warn("Retrying to fetch data from TUB")));
   }
 
-  private ExchangeFilterFunction logRequest(String text) {
+  private ExchangeFilterFunction logRequest() {
     return (clientRequest, nextFilter) -> {
-      log.info(text + clientRequest.url());
+      log.info("Fetching data from TUB: " + clientRequest.url());
       return nextFilter.exchange(clientRequest);
     };
   }
--- src/main/java/org/tub/tubtextservice/model/property/QueryProperties.java
+++ src/main/java/org/tub/tubtextservice/model/property/QueryProperties.java
@@ -1,3 +1,3 @@
 package org.tub.tubtextservice.model.property;
 
-public record QueryProperties(String titles, String authors) {}
+public record QueryProperties(String titles, String authors, String manuscripts, String editions) {}
