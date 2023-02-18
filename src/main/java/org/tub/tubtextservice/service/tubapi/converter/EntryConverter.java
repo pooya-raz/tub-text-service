6aee0382309eebf623ddcba8952e1560a515cfe2
Christopher Pooya Razavian <pooya72@gmail.com>
2023-02-18 10:10:28 +0000

    Convert printouts to entries

--- src/main/java/org/tub/tubtextservice/service/tubapi/converter/EntryConverter.java
+++ src/main/java/org/tub/tubtextservice/service/tubapi/converter/EntryConverter.java
@@ -1,11 +1,78 @@
 package org.tub.tubtextservice.service.tubapi.converter;
 
+import org.tub.tubtextservice.model.domain.Edition;
 import org.tub.tubtextservice.model.domain.Entry;
+import org.tub.tubtextservice.model.domain.Manuscript;
+import org.tub.tubtextservice.model.domain.TitleType;
+import org.tub.tubtextservice.model.domain.person.Author;
+import org.tub.tubtextservice.model.domain.person.Person;
 import org.tub.tubtextservice.service.tubapi.model.TubPrintOuts;
+import org.tub.tubtextservice.service.tubapi.model.tubresponse.MediaWikiPageDetails;
+import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.AuthorPrintouts;
+import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.EditionPrintouts;
+import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.ManuscriptPrintouts;
 import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.TitlePrintouts;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public class EntryConverter {
+
+  private final ManuscriptConverter manuscriptConverter;
+  private final TubDateConverter tubDateConverter;
+
+  private final EditionConverter editionConverter;
+
+  public EntryConverter(
+      ManuscriptConverter manuscriptConverter,
+      TubDateConverter tubDateConverter,
+      EditionConverter editionConverter) {
+    this.manuscriptConverter = manuscriptConverter;
+    this.tubDateConverter = tubDateConverter;
+    this.editionConverter = editionConverter;
+  }
+
   public Entry convert(TitlePrintouts title, final TubPrintOuts printOuts) {
-    return null;
+    final var person = getPerson(printOuts.authors(), title.author().get(0));
+    final var titleName = title.titleTransliterated().stream().findFirst().orElse("");
+    final var titleOriginal = title.titleArabic().stream().findFirst().orElse("");
+    final var manuscripts = getManuscripts(printOuts.manuscripts(), titleName);
+    final var editions = getEditions(printOuts.editions(), titleName);
+    final var titleType =
+        TitleType.valueOfTub(title.bookType().stream().findFirst().orElse("Unknown"));
+    return new Entry(titleName, titleOriginal, person, manuscripts, editions, titleType);
+  }
+
+  private Person getPerson(
+      final Map<String, ArrayList<AuthorPrintouts>> authorPrintoutsMap,
+      final MediaWikiPageDetails authorPage) {
+    if (authorPage == null) {
+      return null;
+    }
+    final var authorName = authorPage.fulltext();
+    final var authorPrintout = authorPrintoutsMap.get(authorName);
+    final var author =
+        authorPrintout.stream().findFirst().orElse(AuthorPrintouts.builder().build());
+    final var personDeath = tubDateConverter.convert(author);
+    return new Author(authorName, personDeath);
+  }
+
+  private List<Manuscript> getManuscripts(
+      final Map<String, ArrayList<ManuscriptPrintouts>> manuscriptPrintoutsMap,
+      final String titleName) {
+    final var manuscriptPrintouts = Optional.ofNullable(manuscriptPrintoutsMap.get(titleName));
+    return manuscriptPrintouts
+        .map(printouts -> printouts.stream().map(manuscriptConverter::convert).toList())
+        .orElseGet(List::of);
+  }
+
+  private List<Edition> getEditions(
+      final Map<String, ArrayList<EditionPrintouts>> editionPrintoutsMap, final String titleName) {
+    final var editionPrintouts = Optional.ofNullable(editionPrintoutsMap.get(titleName));
+    return editionPrintouts
+        .map(printouts -> printouts.stream().map(editionConverter::convert).toList())
+        .orElseGet(List::of);
   }
 }
--- src/test/java/org/tub/tubtextservice/service/tubapi/converter/EntryConverterTest.java
+++ src/test/java/org/tub/tubtextservice/service/tubapi/converter/EntryConverterTest.java
@@ -1,6 +1,11 @@
 package org.tub.tubtextservice.service.tubapi.converter;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.tub.tubtextservice.model.domain.Edition;
 import org.tub.tubtextservice.model.domain.Entry;
 import org.tub.tubtextservice.model.domain.Manuscript;
@@ -15,63 +20,166 @@ import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.Edition
 import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.ManuscriptPrintouts;
 import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.TitlePrintouts;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class EntryConverterTest {
+  public static final String TITLE_TRANSLITERATED = "titleTransliterated";
+  public static final String TITLE_ARABIC = "titleArabic";
+  public static final String YEAR = "687";
+  public static final String GREGORIAN = "1288";
+  private final TitlePrintouts TITLE =
+      TitlePrintouts.builder()
+          .titleTransliterated(TITLE_TRANSLITERATED)
+          .category(MediaWikiPageDetails.builder().fulltext("Edited").build())
+          .titleArabic(TITLE_ARABIC)
+          .bookType("Monograph")
+          .author("author")
+          .build();
+  private final ManuscriptPrintouts MANUSCRIPT_PRINTOUT =
+      ManuscriptPrintouts.builder()
+          .manuscriptOfTitle(TITLE_TRANSLITERATED)
+          .city("city")
+          .location("location")
+          .yearHijri(687)
+          .yearGregorian(1288)
+          .build();
+  private final EditionPrintouts EDITION_PRINTOUT =
+      EditionPrintouts.builder()
+          .publishedEditionOfTitle(TITLE_TRANSLITERATED)
+          .titleTransliterated(TITLE_TRANSLITERATED)
+          .yearHijri(687)
+          .yearGregorian(1288)
+          .build();
+  private final AuthorPrintouts AUTHOR_PRINTOUT =
+      AuthorPrintouts.builder()
+          .deathHijri(687)
+          .deathGregorian(1288L)
+          .fullNameTransliterated("author")
+          .build();
+  private final Author AUTHOR = new Author("author", new HijriDeath(YEAR, GREGORIAN));
+  private final Manuscript MANUSCRIPT =
+      new Manuscript(TITLE_TRANSLITERATED, "city", "location", new HijriDate(YEAR, GREGORIAN));
+  private final Edition EDITION =
+      new Edition(TITLE_TRANSLITERATED, null, null, null, null, null, null, null);
+  private final Map<String, ArrayList<AuthorPrintouts>> mapAuthorPrintout = new HashMap<>();
+  private final Map<String, ArrayList<ManuscriptPrintouts>> mapManuscriptPrintout = new HashMap<>();
+  private final Map<String, ArrayList<EditionPrintouts>> mapEditionPrintout = new HashMap<>();
+  private EntryConverter subject;
+  @Mock private ManuscriptConverter manuscriptConverter;
+  @Mock private TubDateConverter tubDateConverter;
+  @Mock private EditionConverter editionConverter;
+  private TubPrintOuts printouts;
 
-  private final EntryConverter entryConverter = new EntryConverter();
+  @BeforeEach
+  void setUpBeforeEach() {
+    subject = new EntryConverter(manuscriptConverter, tubDateConverter, editionConverter);
+    final var authorList = new ArrayList<AuthorPrintouts>();
+    authorList.add(AUTHOR_PRINTOUT);
+    mapAuthorPrintout.put("author", authorList);
+
+    final var mapList = new ArrayList<ManuscriptPrintouts>();
+    mapList.add(MANUSCRIPT_PRINTOUT);
+    mapManuscriptPrintout.put(TITLE_TRANSLITERATED, mapList);
+
+    final var editionList = new ArrayList<EditionPrintouts>();
+    editionList.add(EDITION_PRINTOUT);
+    mapEditionPrintout.put(TITLE_TRANSLITERATED, editionList);
+
+    printouts =
+        new TubPrintOuts(
+            List.of(TITLE), mapAuthorPrintout, mapManuscriptPrintout, mapEditionPrintout);
+  }
+
+  @AfterEach
+  void afterEach() {
+    mapAuthorPrintout.clear();
+    mapManuscriptPrintout.clear();
+    mapEditionPrintout.clear();
+  }
 
   @Test
   void convertShouldReturnEntry() {
-    final var title =
-        TitlePrintouts.builder()
-            .titleTransliterated("titleTransliterated")
-            .category(MediaWikiPageDetails.builder().fulltext("Edited").build())
-            .titleArabic("titleArabic")
-            .bookType("Monograph")
-            .author("author")
-            .build();
-    final var manuscriptPrintout =
-        ManuscriptPrintouts.builder()
-            .manuscriptOfTitle("titleTransliterated")
-            .city("city")
-            .location("location")
-            .yearHijri(687)
-            .yearGregorian(1288)
-            .build();
-    final var editionPrintout =
-        EditionPrintouts.builder()
-            .publishedEditionOfTitle("titleTransliterated")
-            .titleTransliterated("titleTransliterated")
-            .yearHijri(687)
-            .yearGregorian(1288)
-            .build();
-    final var authorPrintout =
-        AuthorPrintouts.builder()
-            .deathHijri(687)
-            .deathGregorian(1288L)
-            .fullNameTransliterated("author")
-            .build();
-    final var mapAuthorPrintout = Map.of("author", authorPrintout);
-    final var mapManuscriptPrintout = Map.of("titleTransliterated", manuscriptPrintout);
-    final var mapEditionPrintout = Map.of("titleTransliterated", editionPrintout);
-    final var printouts =
-        new TubPrintOuts(
-            List.of(title), mapAuthorPrintout, mapManuscriptPrintout, mapEditionPrintout);
-    final var actual = entryConverter.convert(title, printouts);
+
     final var expected =
         new Entry(
-            "titleTransliterated",
-            "titleArabic",
-            new Author("author", new HijriDeath("687", "1288")),
-            List.of(
-                new Manuscript(
-                    "titleTransliterated", "city", "location", new HijriDate("684", "1288"))),
-            List.of(new Edition("titleTransliterated", null, null, null, null, null, null, null)),
+                TITLE_TRANSLITERATED,
+                TITLE_ARABIC,
+            AUTHOR,
+            List.of(MANUSCRIPT),
+            List.of(EDITION),
             TitleType.Monograph);
+
+    when(manuscriptConverter.convert(MANUSCRIPT_PRINTOUT)).thenReturn(MANUSCRIPT);
+    when(tubDateConverter.convert(AUTHOR_PRINTOUT)).thenReturn(new HijriDeath(YEAR, GREGORIAN));
+    when(editionConverter.convert(EDITION_PRINTOUT)).thenReturn(EDITION);
+    final var actual = subject.convert(TITLE, printouts);
     assertThat(actual).isEqualTo(expected);
   }
+
+  @Test
+  void convertShouldReturnEntryForMultipleEditions() {
+
+    mapEditionPrintout.get(TITLE_TRANSLITERATED).add(EDITION_PRINTOUT);
+
+    final var expected =
+        new Entry(
+                TITLE_TRANSLITERATED,
+                TITLE_ARABIC,
+            AUTHOR,
+            List.of(MANUSCRIPT),
+            List.of(EDITION, EDITION),
+            TitleType.Monograph);
+
+    when(manuscriptConverter.convert(MANUSCRIPT_PRINTOUT)).thenReturn(MANUSCRIPT);
+    when(tubDateConverter.convert(AUTHOR_PRINTOUT)).thenReturn(new HijriDeath(YEAR, GREGORIAN));
+    when(editionConverter.convert(EDITION_PRINTOUT)).thenReturn(EDITION);
+    final var actual = subject.convert(TITLE, printouts);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void convertShouldReturnEntryForMultipleManuscripts() {
+
+    mapManuscriptPrintout.get(TITLE_TRANSLITERATED).add(MANUSCRIPT_PRINTOUT);
+
+    final var expected =
+        new Entry(
+                TITLE_TRANSLITERATED,
+                TITLE_ARABIC,
+            AUTHOR,
+            List.of(MANUSCRIPT, MANUSCRIPT),
+            List.of(EDITION),
+            TitleType.Monograph);
+
+    when(manuscriptConverter.convert(MANUSCRIPT_PRINTOUT)).thenReturn(MANUSCRIPT);
+    when(tubDateConverter.convert(AUTHOR_PRINTOUT)).thenReturn(new HijriDeath(YEAR, GREGORIAN));
+    when(editionConverter.convert(EDITION_PRINTOUT)).thenReturn(EDITION);
+    final var actual = subject.convert(TITLE, printouts);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+    @Test
+  void convertShouldReturnEntryWithoutEditionsOrManuscripts(){
+    mapManuscriptPrintout.clear();
+    mapEditionPrintout.clear();
+
+    final var expected = new Entry(
+            TITLE_TRANSLITERATED,
+            TITLE_ARABIC,
+        AUTHOR,
+        List.of(),
+        List.of(),
+        TitleType.Monograph);
+
+    when(tubDateConverter.convert(AUTHOR_PRINTOUT)).thenReturn(new HijriDeath(YEAR, GREGORIAN));
+    final var actual = subject.convert(TITLE, printouts);
+    assertThat(actual).isEqualTo(expected);
+    }
 }
