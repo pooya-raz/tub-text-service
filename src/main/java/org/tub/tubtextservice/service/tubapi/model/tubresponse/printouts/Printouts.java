590a0e96f3ddc3825f8f273557b63bf77d569efe
Christopher Pooya Razavian <pooya72@gmail.com>
2023-02-19 00:26:40 +0000

    Add documentation

--- src/main/java/org/tub/tubtextservice/service/tubapi/model/tubresponse/printouts/Printouts.java
+++ src/main/java/org/tub/tubtextservice/service/tubapi/model/tubresponse/printouts/Printouts.java
@@ -1,4 +1,9 @@
 package org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts;
 
-public sealed interface Printouts
-        permits DatedPrintouts, TitlePrintouts, PersonPrintouts {}
+/**
+ * Interface for all printouts. Printouts are the data returned by Semantic Mediawiki.
+ *
+ * @see <a href="https://www.semantic-mediawiki.org/wiki/Help:Displaying_information">Semantic
+ *     Mediawiki - Displaying Information</a>
+ */
+public sealed interface Printouts permits DatedPrintouts, TitlePrintouts, PersonPrintouts {}
