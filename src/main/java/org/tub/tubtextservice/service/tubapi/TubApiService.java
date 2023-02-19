163c0747a982f164a53914bde0532af425b42068
Christopher Pooya Razavian <pooya72@gmail.com>
2023-02-19 00:26:58 +0000

    Refactor to use inner classes for better code organization.

--- src/main/java/org/tub/tubtextservice/service/tubapi/TubApiService.java
+++ src/main/java/org/tub/tubtextservice/service/tubapi/TubApiService.java
@@ -21,12 +21,9 @@ import java.util.stream.Stream;
 
 /** The service responsible for retrieving data from the TUB API. */
 public class TubApiService {
-  public static final int STOP = 0;
-  private static final String ACTION_ASK = "ask";
-  private static final String FORMAT_JSON = "json";
+
   private final TubClient tubClient;
   private final TubProperties tub;
-  private int offset = STOP;
 
   public TubApiService(TubClient tubClient, TubProperties tub) {
     this.tubClient = tubClient;
@@ -37,7 +34,7 @@ public class TubApiService {
    * Retrieves the data from the TUB API. Separate queries are required for each category which are
    * organised as {@link Printouts}.
    *
-   * @return the {@link Printouts} from the TUB API as maps with the {@link Data#fullText()} as they
+   * @return the {@link Printouts} from the TUB API as maps with the {@link Data#fullText()} as the
    *     key. The fulltext is the title of the Wiki article, and is used as a unique identifier by
    *     MediaWiki.
    */
@@ -50,80 +47,116 @@ public class TubApiService {
     return new TubPrintOuts(titles, authors, manuscripts, editions);
   }
 
+  /**
+   * Main workflow for retrieving the printouts from the TUB API. Gets the data from the TUB API and
+   * then converts it to a map.
+   *
+   * @param query the Semantic MediaWiki query to be executed.
+   * @param printoutsClass the class of the {@link Printouts} to be cast to.
+   * @return a map of the {@link Printouts}. See {@link MapCreator}.
+   */
   private <T extends Printouts> Map<String, ArrayList<T>> getMapPrintouts(
       String query, Class<T> printoutsClass) {
-    final var data = getAllData(query);
-    return createMap(data, printoutsClass);
+    final var dataFetcher = new TubDataFetcher();
+    final var data = dataFetcher.getAllData(query);
+    return MapCreator.createMap(data, printoutsClass);
   }
 
   /**
    * Creates a map with the {@link Data#fullText()} as the key and the {@link Printouts} as the
-   * value.
-   *
-   * @param dataList the list of {@link Data} to be converted to a map.
-   * @param printoutsClass the class of the {@link Printouts} to be cast to.
-   * @return the map with the {@link Data#fullText()} as the key and the {@link Printouts} as the
-   *     value.
-   * @param <T> the type of {@link Printouts} to be cast to.
+   * value
    */
-  private <T extends Printouts> Map<String, ArrayList<T>> createMap(
-      @NonNull final List<Data> dataList, @NonNull final Class<T> printoutsClass) {
-    final var map = new HashMap<String, ArrayList<T>>();
-    dataList.forEach(
-        data -> {
-          final var printout = printoutsClass.cast(data.printouts());
-          final var key = data.fullText();
-          addToMap(map, key, printout);
-        });
-    return map;
-  }
-
-  private <T extends Printouts> void addToMap(
-      HashMap<String, ArrayList<T>> map, String key, T printout) {
-    if (!map.containsKey(key)) {
-      final var list = new ArrayList<T>();
-      list.add(printout);
-      map.put(key, list);
-    } else {
-      map.get(key).add(printout);
+  private static class MapCreator {
+    /**
+     * Creates a map with the {@link Data#fullText()} as the key and the {@link Printouts} as the
+     * value.
+     *
+     * @param dataList the list of {@link Data} to be converted to a map.
+     * @param printoutsClass the class of the {@link Printouts} to be cast to.
+     * @return the map with the {@link Data#fullText()} as the key and the {@link Printouts} as the
+     *     value.
+     */
+    private static <T extends Printouts> Map<String, ArrayList<T>> createMap(
+        @NonNull final List<Data> dataList, @NonNull final Class<T> printoutsClass) {
+      final var map = new HashMap<String, ArrayList<T>>();
+      dataList.forEach(
+          data -> {
+            final var printout = printoutsClass.cast(data.printouts());
+            final var key = data.fullText();
+            addToMap(map, key, printout);
+          });
+      return map;
+    }
+    /**
+     * Adds the {@code printouts} to the {@code map} with the given {@code key}. Adds to the list if
+     * the key already exists.
+     *
+     * @param map the map to add the {@link Printouts} to.
+     * @param key must be {@link Data#fullText()}.
+     * @param printouts the {@link Printouts} to add to the map.
+     */
+    private static <T extends Printouts> void addToMap(
+        HashMap<String, ArrayList<T>> map, String key, T printouts) {
+      if (!map.containsKey(key)) {
+        final var list = new ArrayList<T>();
+        list.add(printouts);
+        map.put(key, list);
+      } else {
+        map.get(key).add(printouts);
+      }
     }
   }
 
-  /**
-   * Concatenates the data from the TUB API that is returned in multiple requests.
-   *
-   * @param query the query as defined by Semantic MediaWiki.
-   * @return the concatenated {@link Data} from the TUB API.
-   */
-  private List<Data> getAllData(final String query) {
-    var list = fetchData(query);
-    while (offset != STOP) {
-      list = Stream.of(list, fetchData(query + "|offset=" + offset)).flatMap(List::stream).toList();
-    }
-    return list;
-  }
+  /** Retrieves the data from the TUB API. */
+  private class TubDataFetcher {
 
-  /**
-   * Fetches the data from TUB API. The data is paginated based on the {@link TubApiService#offset}.
-   * The offset is initial set to 0 and is updated with the value of returned by the TUB API in
-   * {@link TubResponse#queryContinueOffset()}.
-   *
-   * @param query the query as defined by Semantic MediaWiki.
-   * @return the {@link Data} from the TUB API.
-   */
-  private List<Data> fetchData(final String query) {
-    if (query == null) return List.of();
-    final var result = tubClient.queryTub(ACTION_ASK, FORMAT_JSON, query).block();
-    return Optional.ofNullable(result)
-        .map(
-            r -> {
-              offset = result.queryContinueOffset();
-              return r.query().results().getDataMap().values().stream().toList();
-            })
-        .orElseGet(
-            () -> {
-              offset = STOP;
-              return List.of();
-            });
+    /** The stop value is used to stop the retrieval of data from the TUB API. */
+    public final int STOP = 0;
+
+    /**
+     * The offset is used to retrieve the data in batches. The offset is the number of items to
+     * skip.
+     */
+    private int offset = STOP;
+
+    /**
+     * Concatenates the data from the TUB API that is returned in multiple requests.
+     *
+     * @param query the query as defined by Semantic MediaWiki.
+     * @return the concatenated {@link Data} from the TUB API.
+     */
+    private List<Data> getAllData(final String query) {
+      var list = fetchData(query);
+      while (offset != STOP) {
+        list =
+            Stream.of(list, fetchData(query + "|offset=" + offset)).flatMap(List::stream).toList();
+      }
+      return list;
+    }
+
+    /**
+     * Fetches the data from TUB API. The data is batched based on the value of the {@link
+     * TubDataFetcher#offset}. The offset is initially set to 0 and is only updated if the response
+     * contains a value for {@link TubResponse#queryContinueOffset()}.
+     *
+     * @param query the query as defined by Semantic MediaWiki.
+     * @return the {@link Data} from the TUB API.
+     */
+    private List<Data> fetchData(final String query) {
+
+      if (query == null) return List.of();
+      final var result = tubClient.queryTub("ask", "json", query).block();
+      return Optional.ofNullable(result)
+          .map(
+              r -> {
+                offset = result.queryContinueOffset();
+                return r.query().results().getDataMap().values().stream().toList();
+              })
+          .orElseGet(
+              () -> {
+                offset = STOP;
+                return List.of();
+              });
+    }
   }
 }
