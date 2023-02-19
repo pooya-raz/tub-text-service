7599cbc45c484d780ff614ed3f5479d28ee9c06c
Christopher Pooya Razavian <pooya72@gmail.com>
2023-02-19 14:53:28 +0000

    Refactor TubPrintouts for correct key value pairing

--- src/main/java/org/tub/tubtextservice/service/tubapi/model/TubPrintouts.java
+++ src/main/java/org/tub/tubtextservice/service/tubapi/model/TubPrintouts.java
@@ -1,5 +1,7 @@
 package org.tub.tubtextservice.service.tubapi.model;
 
+import org.tub.tubtextservice.model.domain.Entry;
+import org.tub.tubtextservice.service.tubapi.model.tubresponse.Data;
 import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.AuthorPrintouts;
 import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.EditionPrintouts;
 import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.ManuscriptPrintouts;
@@ -8,9 +10,24 @@ import org.tub.tubtextservice.service.tubapi.model.tubresponse.printouts.TitlePr
 import java.util.ArrayList;
 import java.util.Map;
 
+/**
+ * A record that represents the data available in the TUB wiki. Each field represents the categories
+ * available in the wiki, and are necessary for the construction of an {@link Entry}.
+ *
+ * @param titles titles in TUB. The key is the title of the Wiki article found in {@link
+ *     Data#fullText()}. Since each {@link Entry} is based on one title, it cannot be a list.
+ * @param authors authors in TUB. The key is the title of the Wiki article found in {@link
+ *     Data#fullText()}.
+ * @param manuscripts manuscripts in TUB. The key is the value in {@link
+ *     ManuscriptPrintouts#manuscriptOfTitle()} which refers to the key of a title. One title can
+ *     have multiple manuscripts.
+ * @param editions editions in TUB. The key is the value in {@link
+ *     EditionPrintouts#publishedEditionOfTitle()} which refers to the key of a title. One title can
+ *     have multiple editions.
+ */
 public record TubPrintouts(
-    Map<String, ArrayList<TitlePrintouts>> titles,
-    Map<String, ArrayList<AuthorPrintouts>> authors,
+    Map<String, TitlePrintouts> titles,
+    Map<String, AuthorPrintouts> authors,
     Map<String, ArrayList<ManuscriptPrintouts>> manuscripts,
     Map<String, ArrayList<EditionPrintouts>> editions)
     implements ApiData {}
