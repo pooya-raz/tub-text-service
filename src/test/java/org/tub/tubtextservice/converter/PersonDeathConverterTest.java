2558d4bd8482e8f72c30e08b19294a6b7571f5ff
Christopher Pooya Razavian <pooya72@gmail.com>
2023-03-05 16:17:46 +0000

    Remove unnecessary test

--- src/test/java/org/tub/tubtextservice/converter/PersonDeathConverterTest.java
+++
@@ -1,7 +0,0 @@
-package org.tub.tubtextservice.converter;
-
-import org.tub.tubtextservice.service.tubdata.converter.TubDateConverter;
-
-class PersonDeathConverterTest {
-  private final TubDateConverter subject = new TubDateConverter();
-}
