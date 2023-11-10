7c91c86769c4295f5f7e8750644f525539e3aa3a
Christopher Pooya Razavian <pooya72@gmail.com>
2023-11-10 11:31:16 +0000

    Display message on error

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,7 @@
 function downloadFile() {
-    // Show loader
     document.getElementById('loader').style.display = 'flex';
     document.getElementById('download-button').style.display = 'none';
 
-    // Fetch the file from "/entry" endpoint
     fetch('/entry')
         .then(response => {
             if (!response.ok) {
@@ -12,7 +10,6 @@ function downloadFile() {
             return response.blob();
         })
         .then(blob => {
-            // Create a link element and trigger a click to download the file
             const url = window.URL.createObjectURL(blob);
             const a = document.createElement('a');
             a.href = url;
@@ -20,14 +17,12 @@ function downloadFile() {
             document.body.appendChild(a);
             a.click();
 
-            // Hide loader after the file is downloaded
             document.getElementById('loader').style.display = 'none';
             document.getElementById('download-button').style.display = 'block';
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
-
-            // Hide loader in case of an error
             document.getElementById('loader').style.display = 'none';
+            document.getElementById('download-error').style.display = 'block';
         });
 }
